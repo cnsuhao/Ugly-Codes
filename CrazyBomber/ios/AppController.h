769f89a91b99199cb60f02629ddb070e37b1b223
Yao Melo <melode11@TPSHA6-AN013.local>
2013-06-18 23:02:22 +0800

    first commit

---
+++ CrazyBomber/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CrazyBomberAppController.h
+//  CrazyBomber
+//
+//  Created by Yao Melo on 6/17/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CrazyBomber/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CrazyBomberAppController.h
+//  CrazyBomber
+//
+//  Created by Yao Melo on 6/17/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
